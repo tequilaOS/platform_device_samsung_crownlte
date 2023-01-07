e73ea4b65971383bfc7ca8252a921a002a483d4b
Oliwier Nogaj <56972326+WindowsDev2021@users.noreply.github.com>
2023-01-07 17:15:03 +0100

    crownlte: TequilaOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crownlte.mk
+    $(LOCAL_DIR)/tequila_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crownlte-eng \
-    aosp_crownlte-user \
-    aosp_crownlte-userdebug
+    tequila_crownlte-eng \
+    tequila_crownlte-user \
+    tequila_crownlte-userdebug
